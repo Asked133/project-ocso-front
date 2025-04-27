5c5c0237da87b896ab33ec030d0c32c1e4833046
Asked133 <dcgomez@outlook.com>
2025-04-26 21:46:27 -0600

    feat: Manager card and content

--- app/dashboard/@locations/_components/LocationCard.tsx
+++ app/dashboard/@locations/_components/LocationCard.tsx
@@ -30,7 +30,7 @@ export default async function LocationCard({
       <CardBody>
         <p className="w-full">
           Manager:
-          <Link href={{ pathname: `/dashboard/managers` }}>
+          <Link href={{ pathname: `/dashboard/managers/${data.manager?.managerId}` }}>
             <b>{data.manager?.managerFullName}</b>
           </Link>
         </p>
