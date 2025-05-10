599256bb40080a94ff3253facccdc2c5b2eac624
Asked133 <dcgomez@outlook.com>
2025-05-10 15:27:19 -0600

    feat: Add Product and FilteredCards components with product fetching logic

--- app/dashboard/products/_components/ProductCard.tsx
+++ app/dashboard/products/_components/ProductCard.tsx
@@ -1,10 +1,9 @@
-
 import { Product } from "@/entities";
 import { Card, CardBody, CardHeader, Divider } from "@heroui/react";
 
 export default function ProductCard ({product} : {product: Product}) {
     return (
-        <Card className="hover:scale-110 max-w-[350px]">
+        <Card className="max-w-[350px]">
             <CardHeader> {product.productName} </CardHeader>
             <Divider/>
             <CardBody>
